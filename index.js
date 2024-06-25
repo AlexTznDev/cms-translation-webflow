bf220edf2f5e7b0a956c5cb25592309757e9e55e
AlexTznDev <alex.tuysuzian91@gmail.com>
2024-06-25 12:44:46 +0200

    Initial commit

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+const port = 3000;
+
+app.use(bodyParser.json()); // pour supporter les corps de requêtes JSON
+app.use(bodyParser.urlencoded({ extended: true })); // pour supporter les corps de requêtes URL-encoded
+
+app.post('/modify-html', (req, res) => {
+    const htmlContent = req.body.html;
+    const modifiedHtml = htmlContent.replace(/'/g, " "); // Exemple de modification
+    res.send(modifiedHtml);
+});
+
+app.listen(port, () => {
+    console.log(`Server running on http://localhost:${port}`);
+});
